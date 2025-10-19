6719e74ae8b491c13953c0b05d6683ecdb86f54e
Kha <Kha999888777666@gmail.com>
2025-10-19 10:39:32 +0700

    Khởi tạo lại code sạch cho EF

---
+++ AirlineReservation/src/AirlineReservation.Presentation (WinForms)/Assets/Color Palettes/Color.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservation.src.AirlineReservation.Presentation__WinForms_.Assets.Color_Palettes
+{
+    internal class Color
+    {
+        public static readonly Color Text1 = Color.FromArgb(0, 0, 0);           // #000000
+        public static readonly Color Button = Color.FromArgb(0, 137, 237);       // #0089ED
+        public static readonly Color Correct2 = Color.FromArgb(0, 138, 96);        // #008A60
+        public static readonly Color NavigatorText = Color.FromArgb(32, 103, 218);      // #2067DA
+        public static readonly Color Correct1 = Color.FromArgb(47, 193, 147);      // #2FC193
+        public static readonly Color Text2 = Color.FromArgb(53, 50, 50);        // #353232
+        public static readonly Color Hyperlink = Color.FromArgb(66, 133, 244);      // #4285F4
+        public static readonly Color Placeholder = Color.FromArgb(128, 128, 128);     // #808080
+        public static readonly Color StrokeBtnOrTB = Color.FromArgb(173, 173, 173);     // #ADADAD
+        public static readonly Color Alert = Color.FromArgb(225, 45, 45);       // #E12D2D
+        public static readonly Color ShapeBox1 = Color.FromArgb(245, 245, 245);     // #F5F5F5
+        public static readonly Color ShapeBox2 = Color.FromArgb(248, 247, 249);     // #F8F7F9
+        public static readonly Color SunsetOrange = Color.FromArgb(255, 148, 18);      // #FF9412
+        public static readonly Color Background = Color.FromArgb(255, 255, 255);     // #FFFFFF
+        private static Color FromArgb(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
